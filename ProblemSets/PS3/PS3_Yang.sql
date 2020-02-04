6515b9ce197d855b668c94199994327ddbf79590
Fuqing-Yang <yang145523@gmail.com>
2020-02-04 14:35:14 -0600

    Completed PS3

---
+++ ProblemSets/PS3/PS3_Yang.sql
@@ -0,0 +1,11 @@
+.mod csv
+.import /home/ouecon040/DScourseS20/FL_insurance_sample.csv datname
+
+SELECT * FROM datname LIMIT 10;
+
+SELECT DISTINCT county FROM datname;
+
+SELECT AVG(tiv_2012 - tiv_2011) FROM datname;
+
+SELECT construction, COUNT(*) FROM datname GROUP BY construction;
+
